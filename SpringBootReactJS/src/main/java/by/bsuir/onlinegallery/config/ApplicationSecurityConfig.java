eed90225958043080e0892fc8c1f44d05b084254
EugeneBlizniuk <janey1102@mail.ru>
2020-05-03 20:40:07 +0300

    configured ApplicationSecurity, created unauthorized handler

--- SpringBootReactJS/src/main/java/by/bsuir/onlinegallery/config/ApplicationSecurityConfig.java
+++ SpringBootReactJS/src/main/java/by/bsuir/onlinegallery/config/ApplicationSecurityConfig.java
@@ -1,17 +1,22 @@
 package by.bsuir.onlinegallery.config;
 
+import by.bsuir.onlinegallery.security.JwtAuthenticationEntryPoint;
 import by.bsuir.onlinegallery.service.impl.CustomUserDetailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -20,11 +25,16 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final CustomUserDetailService userService;
     private final PasswordEncoder passwordEncoder;
+    private final JwtAuthenticationEntryPoint unauthorizedHandler;
 
     @Autowired
-    public ApplicationSecurityConfig(CustomUserDetailService userService, PasswordEncoder passwordEncoder) {
+    public ApplicationSecurityConfig(CustomUserDetailService userService,
+                                     PasswordEncoder passwordEncoder,
+                                     JwtAuthenticationEntryPoint unauthorizedHandler) {
+
         this.userService = userService;
         this.passwordEncoder = passwordEncoder;
+        this.unauthorizedHandler = unauthorizedHandler;
     }
 
     @Override
@@ -32,6 +42,40 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
         authenticationManagerBuilder
                 .userDetailsService(userService)
                 .passwordEncoder(passwordEncoder);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .cors()
+                    .and()
+                .csrf().disable()
+                .exceptionHandling().authenticationEntryPoint(unauthorizedHandler)
+                    .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                    .and()
+                .authorizeRequests()
+                .antMatchers("/",
+                        "/favicon.ico",
+                        "/**/*.png",
+                        "/**/*.gif",
+                        "/**/*.svg",
+                        "/**/*.jpg",
+                        "/**/*.html",
+                        "/**/*.css",
+                        "/**/*.js")
+                .permitAll()
+                .antMatchers("/api/auth/**")
+                .permitAll()
+                .antMatchers("/api/user/checkUsernameAvailability", "/api/user/checkEmailAvailability")
+                .permitAll()
+                .antMatchers(HttpMethod.GET, "/api/polls/**", "/api/users/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated();
+
+//        TODO: I might need it, not sure right now
+//        http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 
     @Bean(BeanIds.AUTHENTICATION_MANAGER)
