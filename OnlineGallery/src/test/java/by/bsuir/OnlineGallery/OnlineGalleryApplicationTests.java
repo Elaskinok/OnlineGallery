ac9417403dc55b7fa313989a9216ee8165f1c134
EugeneBlizniuk <janey1102@mail.ru>
2020-05-05 20:13:22 +0300

    generated brand new project

---
+++ OnlineGallery/src/test/java/by/bsuir/OnlineGallery/OnlineGalleryApplicationTests.java
@@ -0,0 +1,13 @@
+package by.bsuir.OnlineGallery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineGalleryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
